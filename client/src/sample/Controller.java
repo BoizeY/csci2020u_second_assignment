58b8937a5b7aa376473b6795522f44b8b9a26d10
Bo <wen.yu@uoit.net>
2018-03-27 19:39:17 -0400

    Implemented getLocalDir and getServerDir

--- client/src/sample/Controller.java
+++ client/src/sample/Controller.java
@@ -14,24 +14,28 @@ import javafx.scene.control.*;
 
 public class Controller {
 
+    // Static variable
+    private static final String LOCALFOLDER = "./src/LocalFolder/";
 
+    // private variables
     private Socket socket = null;
     private BufferedReader in = null;
     private PrintWriter clientOut = null;
     private BufferedReader clientIn = null;
 
-    @FXML
-    ListView serverFileList;
-    List<String> dirList = new ArrayList<>();
-    protected ListProperty<String> listProperty = new SimpleListProperty<>();
+    //FMXL variables
+    @FXML ListView serverFileList;
+    List<String> serverDirList = new ArrayList<>();
+    protected ListProperty<String> serverListProperty = new SimpleListProperty<>();
+    @FXML ListView localFileList;
+    List<String> localDirList = new ArrayList<>();
+    protected  ListProperty<String> localListProperty = new SimpleListProperty<>();
 
 
 
     public void initialize() {
-      // getDir();
-
-        
-
+        getDir();
+        getLocalDir();
     }
 
     @FXML protected void upload(ActionEvent event) {
@@ -57,23 +61,55 @@ public class Controller {
             String input = "";
             input = clientIn.readLine();
 
-            //convert input into a list
-            String[]  dirArray = input.split(",");
-
-            for (int i =0; i < dirArray.length; i++){
-
-                dirList.add(dirArray[i]);
+            //if there is no file in the server, close socket and exit function
+            if( input == "EMPTY"){
+                socket.close();
+                return;
             }
+            // if there is file in the server pull and update list
+            else {
 
-            serverFileList.itemsProperty().bind(listProperty);
+                //convert input into a list
+                String[] dirArray = input.split(",");
 
-            listProperty.set(FXCollections.observableArrayList(dirList));
+                for (int i = 0; i < dirArray.length; i++) {
+                    serverDirList.add(dirArray[i]);
+                }
 
+                //set list of server file to list view
+                serverFileList.itemsProperty().bind(serverListProperty);
+                serverListProperty.set(FXCollections.observableArrayList(serverDirList));
+            }
             //close socket
             socket.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void getLocalDir(){
+
+        //get the local dir
+        File localFolder = new File(LOCALFOLDER);
+
+        //if there is no file exit function
+        if(localFolder == null) {
+            return;
+        }
+
+        //if there is files populate the list
+        else {
+
+            String[] localFiles = localFolder.list();
+
+            for (int i = 0; i < localFiles.length; i++) {
+                localDirList.add(localFiles[i]);
+            }
+
+            localFileList.itemsProperty().bind(localListProperty);
+            localListProperty.set(FXCollections.observableArrayList(localDirList));
+        }
+
     }
 }
